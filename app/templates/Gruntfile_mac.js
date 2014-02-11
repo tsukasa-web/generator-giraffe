0292464a2f168d35c9baf8740f610d8bd0785fd9
tsukasa-oneslocus <tokura-tsukasa@tokura-tsukasa-no-MacBook-Air.local>
2014-02-11 23:16:57 +0900

    各種調整

--- app/templates/Gruntfile_mac.js
+++ app/templates/Gruntfile_mac.js
@@ -89,7 +89,7 @@ module.exports = function(grunt) {
 			},
 			run: {
 				src: [
-					'<%%= path.root %>/<%%= path.src %>/js/lib/jquery.min.js',
+					'<%%= path.root %>/<%%= path.src %>/lib/jquery.min.js',
 					'<%%= path.root %>/<%%= path.src %>/js/dest/hogehoge.js'
 				],
 				dest: '<%%= path.root %>/<%%= path.src %>/js/run-all.js'
